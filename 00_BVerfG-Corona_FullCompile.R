a4e417f6888ebc1a4cca3b37bace87bf51d2e270
seanfobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-02-03 14:59:12 +0100

    add compile script

---
+++ 00_BVerfG-Corona_FullCompile.R
@@ -0,0 +1,51 @@
+#!/bin/Rscript
+
+#'# Vorbereitung
+
+
+datestamp <- Sys.Date()
+
+library(rmarkdown)
+
+
+
+
+#'# Aufräumen
+
+
+files.delete <- list.files(pattern = "\\.zip|\\.jpe?g|\\.png|\\.gif|\\.pdf|\\.txt|\\.bib|\\.csv|\\.spin\\.|\\.log|\\.html?",
+                           ignore.case = TRUE)
+
+unlink(files.delete)
+
+unlink("output", recursive = TRUE)
+unlink("analyse", recursive = TRUE)
+unlink("ANALYSE", recursive = TRUE)
+unlink("temp", recursive = TRUE)
+unlink("data", recursive = TRUE)
+
+
+
+
+
+
+#+
+#'# Datensatz 
+#' 
+#' Um den **vollständigen Datensatz** zu kompilieren und einen PDF-Bericht zu erstellen, kopieren Sie bitte alle im Source-Archiv bereitgestellten Dateien in einen leeren Ordner und führen mit R diesen Befehl aus:
+
+
+begin.compreport <- Sys.time()
+
+rmarkdown::render(input = "BVerfG-Corona_CorpusCreation.R",
+                  envir = new.env(),
+                  output_file = paste0("BVerfG-Corona_",
+                                       datestamp,
+                                       "_CompilationReport.pdf"),
+                  output_dir = "output")
+
+
+end.compreport <- Sys.time()
+
+print(end.compreport-begin.compreport)
+
