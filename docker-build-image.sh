4341c31976e3b61e54202000435d4b3d15513c4c
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2024-09-15 19:16:49 +0200

    update R and rocker to 4.4.0

--- docker-build-image.sh
+++ docker-build-image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-time docker build -t bverfg-corona:4.2.2 .
+time docker build -t bverfg-corona:4.4.0 .
--- docker-run-project.sh
+++ docker-run-project.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-time docker build -t bverfg-corona:4.2.2 .
+time docker build -t bverfg-corona:4.4.0 .
 
 time docker-compose run --rm bverfg-corona Rscript run_project.R
